368adea27cacce7211482b2d5988b233f8f87aaa
Grant Lubsch <grant.lubsch@gmail.com>
2023-03-22 23:20:30 -0500

    added FilterableContactTable component

--- src/App.js
+++ src/App.js
@@ -69,4 +69,44 @@ class SearchBar extends React.Component {
   }
 }
 
-export default App;
+class FilterableContactTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      contacts : []
+    };
+    this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
+    this.addContact = this.addContact.bind(this);
+  }
+
+  addContact(contact) {
+    var timestamp = new Date().getTime();
+    contact['key'] = timestamp;
+    this.state.contacts.push(contact);
+    this.setState({ contacts: this.state.contacts });
+  }
+
+  handleFilterTextInput(filterText) {
+    this,setState({
+      filterText: filterText
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Contact List</h1>
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextInput={this.handleFilterTextInput}}
+          />
+          <NewContactRow addContact={this.addContact}/>
+          <ContactTable
+            contacts={this.state.contacts}
+            filterText={this.state.filterText}
+          />
+      </div>
+    );
+  }
+}
