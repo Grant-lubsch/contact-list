f4dd9791e899401351ac7da8a8c8a67910d57735
Grant Lubsch <grant.lubsch@gmail.com>
2023-03-22 22:40:33 -0500

    reworks contacts component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from "react";
 import "./App.css";
-import Home from "./home";
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+class ContactRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.contact.name}</td>
+        <td>{this.props.contact.phone}</td>
+        <td>{this.props.contact.email}</td>
+      </tr>
+    );
+  }
 }
 
+class ContactTable extends React.Component {
+  render() {
+    var rows = [];
+    this.props.contacts.forEach((contact) => {
+      if (contact.name.indexOf(this.props.filterText) === -1) {
+        return;
+      }
+      rows.push(<contactRow key={contact.key} contact={contact} />);
+    });
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>
+              <i className="fa fa-fw fa-user"></i>Name
+            </th>
+            <th>
+              <i className="fa fa-fw fa-phone"></i>Phone
+            </th>
+            <th>
+              <i className="fa fa-fw fa-email"></i>E-Mail
+            </th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
 export default App;
