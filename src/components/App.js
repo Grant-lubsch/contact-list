048bbfe58c869a9be3e8951de6d487bd9ea66b51
Grant Lubsch <grant.lubsch@gmail.com>
2023-03-26 14:50:37 -0500

    added view-contact

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 //import ReactDOM from "react-dom";
-import "./App.css";
-import Home from "./Home";
+import "../App.css";
+import Home from "./home.js";
 
 function App() {
   return (
